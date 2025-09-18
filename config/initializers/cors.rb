4683406f6df21a2766c74c307f802134b2bd2702
h-ichi <h.ichimiya.dosapo@gmail.com>
2025-09-18 10:53:56 +0900

    cors修正

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -19,7 +19,7 @@
 # config/initializers/cors.rb
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'  # 開発中は*、本番はReactのURLに置き換え
+    origins 'https://aki-finder-app.onrender.com/'  # 開発中は*、本番はReactのURLに置き換え
     resource '*', headers: :any, methods: [:get, :post, :options]
   end
 end
